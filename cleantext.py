5c1b7b4c8a1d8d79f24eba1bd8993d641b4c6c53
marinarosell <48138799+marinarosell@users.noreply.github.com>
2019-10-12 14:12:45 +0200

    now you can train!

--- cleantext.py
+++ cleantext.py
@@ -1,8 +1,32 @@
 from tika import parser
 
-raw = parser.from_file('text1.pdf')
-s = (raw['content'])
-s = str(s)
-s = s.replace(",","")
-s = s.replace(".","")
-print (s)
+class ArrangedText:
+    def __init__(self, file):
+        text_string = ""
+        if(file[-3:] == 'pdf'):
+            raw = parser.from_file(file)
+            text_string = (raw['content'])
+            text_string = str(text_string)
+        elif(file[-3:] == 'txt'):
+            with open(file, 'r') as txt_file:
+                text_string = txt_file.read()
+                print(text_string)
+        arr = self._clean(text_string)
+        self.l = text_list = self._getList(arr)
+        print(text_list)
+
+    def _clean(self, text):
+        text = text.replace(",","").replace("."," .").lower().replace("?"," ?").replace("¿", "").replace("!", " !").replace("¡", "").replace("(", "").replace(")","").replace("[","").replace("]","").replace("{","").replace("}","").replace("\n", " ").replace("  ", " ").replace(":", "").replace(";", "")
+        text = text.replace("1", "").replace("2", "").replace("3", "").replace("4", "").replace("5", "").replace("6", "").replace("7", "").replace("8", "").replace("9", "").replace("0", "")
+        return text
+
+    def _getList(self, text):
+        li = list(text.split(" "))
+        return li
+
+    def numberWords(self):
+        print(len(self.l))
+
+    def numberDifWords(self):
+        text_set = set(self.l)
+        print(len(text_set))
